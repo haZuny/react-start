25ca61e7517876949de66c400488fca0e48981dd
haZuny <gkwns5791@naver.com>
2024-07-17 17:00:36 +0900

    state hooks

--- createtest/src/component/Hello.js
+++ createtest/src/component/Hello.js
@@ -1,14 +1,18 @@
 import s from './Hello.module.css'
+import { useState } from 'react'
 
-function showname(e){
-    console.log(e.target.text)
-}
+
 
 export default function(){
+    const [name, setName] = useState("하준")
+
+    function showname(e){
+        setName(name === "하준" ? "준하":"하준")
+    }
+
+
     return <div>
-        <h1 onClick={showname}>hello component1</h1>
+        <h1 >{name}</h1>
         <button onClick={showname}>btn1</button>
-        <button>btn2</button>
-        <input onChange={showname}></input>
     </div>
 }
